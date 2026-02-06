3c897efd81b29f79f999c3fcf57358c5166ee9d8
olsenabi32 <olgasteroescobar@gmail.com>
2026-02-06 12:51:29 +0100

    funcionamiento indice

--- Ayuda/help_set.hs
+++ Ayuda/help_set.hs
@@ -7,9 +7,16 @@
   </maps>
 
   <view>
-    <name>Tabla Contenidos</name>
-    <label>Contenidos</label>
+    <name>TOC</name>
+    <label>Tabla de Contenidos</label>
     <type>javax.help.TreeView</type>
     <data>toc.xml</data>
+  </view>
+
+  <view>
+    <name>Indice</name>
+    <label>Indice</label>
+    <type>javax.help.IndexView</type>
+    <data>indice.xml</data>
   </view>
 </helpset>
